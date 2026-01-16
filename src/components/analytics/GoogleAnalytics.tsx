166c65d57d081b2846121a13e8e671c50d11f003
litheonhq <litheonhq@gmail.com>
2026-01-16 10:24:22 +0900

    feat(analytics): add GA4 tracking for public pages

---
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+
+export function GoogleAnalytics() {
+  if (!GA_MEASUREMENT_ID) {
+    return null;
+  }
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga4-init" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', '${GA_MEASUREMENT_ID}', { anonymize_ip: true });`}
+      </Script>
+    </>
+  );
+}
